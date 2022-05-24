348cb094b2dca18b265732314ffd44d482edefb2
bozoputer <e@ekathuria.dev>
2022-05-24 10:15:28 -0400

    Finish initial set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,38 @@
-module.exports = {
-  // inform webpack of the app entrypoint
-  entry: './src/index.js'
-}
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports= {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'index.html'),
+    },
+    compress: true,
+    port: 8080
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      hash: true,
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ],
+  },
+};
