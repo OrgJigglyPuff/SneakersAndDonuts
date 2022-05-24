7ba459a7693406606289dff54f75d8a1c93564d9
Alvin Ma <alvin.ma95@gmail.com>
2022-05-23 22:42:11 -0400

    connect app to datbases and add signup functionality

---
+++ server/models/shoppingCartModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+const myURI = process.env.MONGODB_URI
+
+
+// @cluster0.onyhb.mongodb.net
+mongoose.connect(myURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: process.env.DB_NAME
+  })
+    .then(() => console.log('Connected to Mongo DB.'))
+    .catch(err => console.log(err));
+const Schema = mongoose.Schema;
+
+const shoppingCartSchema = new Schema({ 
+  items: {type: Array},
+  total_quantity: {type: Number},
+  total_price: {type: Number},
+});
+
+const ShoppingCart = mongoose.model('ShoppingCart', shoppingCartSchema);
+
+module.exports = ShoppingCart;
