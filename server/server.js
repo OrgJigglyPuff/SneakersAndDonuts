3dceda29bc17ad95d94683f9d68910dee192e100
Alvin Ma <alvin.ma95@gmail.com>
2022-05-24 12:29:10 -0400

    add getProducts functionality from store route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const authRoute = require('./routes/loginSignUp')
+const storeRoute = require('./routes/storeRoute')
 
 const app = express();
 const PORT = 3000;
@@ -10,7 +11,8 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 app.get('/',(req,res)=>{ return res.status(200).sendFile(path.resolve(__dirname,'../src/index.html'))})
-app.use('/signup',authRoute)
+app.use('/auth',authRoute)
+app.use('/store',storeRoute)
 
 
 
@@ -18,4 +20,17 @@ app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
 
+const globalErrorHandler = (err,req,res,next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 404,
+    message: {err: 'an error occured'}
+  }
+
+  const errObj = Object.assign({},defaultErr,err);
+  console.log(errObj.log);
+  res.status(errObj.status).json(errObj.messages);
+}
+
+app.use(globalErrorHandler);
 module.exports = app;
