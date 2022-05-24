2be304d6bdd704fa3a70264138aa5d32f8eab762
Alvin Ma <alvin.ma95@gmail.com>
2022-05-24 16:10:30 -0400

    include shopping cart get, add, delete methods

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -2,11 +2,31 @@ const Store = require('../models/storeModel.js')
 const ShoppingCarts = require('../models/shoppingCartModel.js')
 const cartController = {};
 
+
+cartController.getItems = async (req, res, next) => {
+  console.log('entered getItems middleware')
+  const {_id } = req.body
+  try{
+  res.locals.items=  await ShoppingCarts.findOne({_id})
+  console.log(res.locals.items)
+  return next()
+      
+    }
+  catch(err){
+    console.log(err)
+    return next({
+      log: `cartController.addProduct: ERROR: ${err}`,
+      message: {err: `Error occurred in cartController.getProduct ${err.error}`}
+    });
+  };
+};
 cartController.addProduct = async (req, res, next) => {
     console.log('entered addProduct middleware')
    let {_id, item, price } = req.body
+
+   if(typeof price === 'string'){
     price = Number(price.slice(1))
-    console.log(typeof price, price)
+   }
    
     try{
     res.locals.shoppingCartItems=  await ShoppingCarts.findOneAndUpdate({_id,_id}, {$push: {"items": item}, $inc: {total_price: price, total_quantity: 1}}, {new:true})
@@ -17,11 +37,49 @@ cartController.addProduct = async (req, res, next) => {
     catch(err){
       console.log(err)
       return next({
-        log: `storeController.getProducts: ERROR: ${err}`,
-        message: {err: `Error occurred in storeController.getProducts ${err.error}`}
+        log: `cartController.addProduct: ERROR: ${err}`,
+        message: {err: `Error occurred in cartController.getProduct ${err.error}`}
       });
     };
   };
+
+  cartController.deleteProduct = async (req,res,next) => {
+    console.log('entered addProduct middleware')
+    let {_id, item, price } = req.body
+ 
+    if(typeof price === 'string'){
+     price = Number(price.slice(1)*-1)
+    }
+    
+     try{
+       await ShoppingCarts.updateOne( {_id,_id},[ 
+        { $set: { 
+             items: {
+                 $let: {
+                     vars: { ix: { $indexOfArray: [ '$items', item ] } },
+                     in: { $concatArrays: [
+                               { $slice: [ '$items', 0, "$$ix"] },
+                               [ ],
+                               { $slice: [ '$items', { $add: [ 1, "$$ix" ] }, { $size: '$items' } ] }
+                          ]
+                     }
+                }
+            }
+        }}
+    ]  )
+     res.locals.shoppingCartItems =  await ShoppingCarts.findOneAndUpdate({_id,_id}, { $inc: {total_price: price, total_quantity: -1}}, {new:true})
+     
+     return next()
+         
+       }
+     catch(err){
+       console.log(err)
+       return next({
+         log: `cartController.deleteProduct: ERROR: ${err}`,
+         message: {err: `Error occurred in cartController.deleteProduct ${err.error}`}
+       });
+     };
+  }
   
   
   module.exports = cartController;
--- server/routes/storeRoute.js
+++ server/routes/storeRoute.js
@@ -8,7 +8,15 @@ router.get('/',storeController.getProducts,(req,res)=> {
     return res.status(200).json(res.locals.products)
 })
 
+router.post('/get', cartController.getItems,(req,res)=> {
+    return res.status(200).json(res.locals.items)
+})
+
 router.post('/add',cartController.addProduct,(req,res)=> {
+    return res.status(201).json(res.locals.shoppingCartItems)
+})
+
+router.post('/delete',cartController.deleteProduct,(req,res)=> {
     return res.status(200).json(res.locals.shoppingCartItems)
 })
 
